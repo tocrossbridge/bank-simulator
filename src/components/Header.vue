eaba18816b1428e8f0cc1d6ea8e1b5a40bbed6cb
bea <beatriztattooer@gmail.com>
2022-07-19 19:12:58 -0300

    ✨ new component: header
    1. Create Header component
    2. Implemented at app

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+import IconLogo from './icons/IconLogo.vue';
+
+export default {
+    components: { IconLogo }
+}
+</script>
+<template>
+    <header>
+        <IconLogo />
+    </header>
+</template>
+<style scoped>
+header {
+    margin: 2em;
+}
+@media screen and (min-width: 900px) {
+    header {
+        max-width: 80%;
+        margin: 2em auto;
+    }
+}
+</style>
