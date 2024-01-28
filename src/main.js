3690c49aa84ca694a868fb0c3fe1f77766379e44
bea <beatriztattooer@gmail.com>
2024-01-28 11:02:06 -0300

    :rocket: install: bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
-
 app.mount("#app");
+
+
+// Make BootstrapVue available throughout your project:
+app.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin:
+app.use(IconsPlugin)
